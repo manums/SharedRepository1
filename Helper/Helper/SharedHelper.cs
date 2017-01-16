5feefafdf25310578f28e6254f03567196ea6ba8
Manu Shivakumar <manu.machilles@outlook.com>
2017-01-16 15:16:22 +0530

    intial Repository

---
+++ Helper/Helper/SharedHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helper
+{
+    public class SharedHelper
+    {
+        public int GetLength(string str)
+        {
+            if (string.IsNullOrEmpty(str)) return 0;
+            return str.Length;
+        }
+    }
+}
---
+++ Helper/SharedLogging/Logger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLogging
+{
+    public class Logger
+    {
+        public void WriteToFile(string filePath, string content)
+        {
+            if (!File.Exists(filePath))
+                return;
+
+            File.AppendAllLines(filePath, new string[] { content });
+        }
+    }
+}
